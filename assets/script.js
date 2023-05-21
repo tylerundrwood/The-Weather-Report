f6d7e0009c84ba03c165d97d9eef76053e5b3c90
Tyler Underwood <twood9600@yahoo.com>
2023-05-21 16:53:25 -0400

    finished styling

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
+let timeEl = document.getElementById('time');
+let dateEl =document.getElementById('date');
+let currentWeatherItemEl = document.getElementById('current-weather-item')
+let timezone = document.getElementById('time-zone');
+let countryEl = document.getElementById('country');
+let weatherForecastEl = document.getElementById('weather-forecast');
+let currentTempEl = document.getElementById('current-temp');
 let APIKey = c84e398e53ea574153ad1bdd9558e008;
 
