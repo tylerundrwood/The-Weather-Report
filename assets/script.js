d53b0980e4a1559ef833e561074d0cf6002a330a
Tyler Underwood <twood9600@yahoo.com>
2023-05-25 21:40:20 -0400

    messed around with api

--- assets/script.js
+++ assets/script.js
@@ -8,10 +8,12 @@ let cityWind = document.getElementById('wind-speed');
 let cityHumidity = document.getElementById('humidity');
 let weatherForecastEl = document.getElementById('weather-forecast');
 let currentTempEl = document.getElementById('current-temp');
+let searchButtonEl = document.getElementById('search-click');
 let APIKey = 'c84e398e53ea574153ad1bdd9558e008';
 let userSearchElValue = document.getElementById('user-search-input').value;
+let searchHistoryListEl = document.getElementById('search-list');
 
-let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 setInterval(() => {
@@ -26,18 +28,18 @@ setInterval(() => {
 
     timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
 
-    dateEl.innerHTML =  days[day] + ',' + date + ' ' + months[months]
+    dateEl.innerHTML =  days[day] + ', ' + date+ ' ' + months[months]
 }, 1000);
 
-searchButtonEl.addEventListener('click', function() {
+    searchButtonEl.addEventListener('click', function() {
 
     let userSearchElValue = document.getElementById('user-search-input').value;
 
-    userInputCity.textContent = userSearchElValue + '   ' + moment().format("dddd, MMMM Do") 
+   // userInputCity.textContent = userSearchElValue + '   ' + moment().format("dddd, MMMM Do") 
     
     fetch(`api.openweathermap.org/data/2.5/forecast?q=${userSearchElValue}units=imperial&appid=${APIKey}`)
     .then(res => res.json()).then(data =>{
-        showWeatherData(data);
+        
          })
         })
 
@@ -79,4 +81,24 @@ data.daily.forEach((day, idx) => {
 })
 
 weatherForecastEl.innerHTML = otherDayForecast;
-}
+};
+searchButtonEl.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    let storedHistoryArr = []
+
+    let storedHistory = document.createElement('button');
+    $(storedHistory).css("width", "80px");
+    $(storedHistory).css("margin-top", "10px");
+
+   let test = document.getElementById('user-search-input').value;
+   localStorage.setItem('test', test);
+
+    storedHistoryArr.pop(test);
+    console.log(storedHistoryArr);
+
+   let tester = localStorage.getItem('test');
+   storedHistory.textContent = tester;
+
+   searchHistoryListEl.append(storedHistory);
+})
